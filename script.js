bb500aae5eb69956716a5ac76acd8bc4b39ce3a3
revania17 <revaniazahrar@gmail.com>
2025-03-16 11:16:26 +0700

    memperbaiki navbar

--- script.js
+++ script.js
@@ -9,3 +9,15 @@ document.addEventListener("DOMContentLoaded", function () {
         }, 100); 
     });
 });
+
+document.addEventListener("DOMContentLoaded", function () {
+    let navbarToggler = document.querySelector(".navbar-toggler");
+    let navbarCollapse = document.querySelector("#navbarNav");
+
+    navbarToggler.addEventListener("click", function () {
+        if (navbarCollapse.classList.contains("show")) {
+            navbarCollapse.classList.remove("show");
+        }
+    });
+});
+
